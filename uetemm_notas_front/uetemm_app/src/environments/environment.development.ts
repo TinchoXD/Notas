e72f90e191cd4e35f281a4011bb176163dfe2c9a
martinb1992 <martinbalarezo92@hotmail.com>
2024-09-23 06:49:47 -0500

    configuracion .ENV (HOST)

--- uetemm_notas_front/uetemm_app/src/environments/environment.development.ts
+++ uetemm_notas_front/uetemm_app/src/environments/environment.development.ts
@@ -1,9 +1,13 @@
 export const environment = {
     production: false,
-    urlHost: 'http://172.16.20.47:8080',
-    urlApi: 'http://172.16.20.47:8080/api/',
-
-
+    //urlHost: 'http://172.16.20.47:8080',
+    //urlApi: 'http://172.16.20.47:8080/api/',
+    
+    //urlHost: 'http://192.168.1.66:8080',
+    //urlApi: 'http://192.168.1.66:8080/api/',
+    
+    urlHost: 'https://www.back.martinbalarezo.tudominio.pro',
+    urlApi: 'https://www.back.martinbalarezo.tudominio.pro/api/',
 
 
 
